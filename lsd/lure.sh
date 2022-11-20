5e3bb2d66469d5dbb0f114151c8cc48d4f178874
SinTan1729 <sayantan.santra689@gmail.com>
2022-11-20 02:37:59 -0600

    Disable verbose decompression

--- lsd/lure.sh
+++ lsd/lure.sh
@@ -21,7 +21,7 @@ package() {
 	vers="$(curl --silent "https://api.github.com/repos/${git_repo}/releases/latest" | grep -Eo '"tag_name": "(.*)"' | sed -E 's/.*"([^"]+)".*/\1/')"
 	echo Installing ${name} ${vers}
 	curl -L "https://github.com/${git_repo}/releases/download/${vers}/${name}-${vers}-x86_64-unknown-linux-musl.tar.gz" -o ${name}.tar.gz
-	tar xzvf "${name}.tar.gz" -C .
+	tar xzf "${name}.tar.gz" -C .
 	cd "./${name}-${vers}-x86_64-unknown-linux-musl"
 	install -Dm755 "./${name}" "${pkgdir}/usr/local/bin/${name}"
 	install -Dm644 "./${name}.1" "${pkgdir}/usr/local/share/man/man1/${name}.1"

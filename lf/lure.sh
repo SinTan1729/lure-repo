ccc6237827940bf792b1174aa66cf5303953b8c4
SinTan1729 <sayantan.santra689@gmail.com>
2022-11-20 13:25:48 -0600

    Added man and autocomplete for lf

--- lf/lure.sh
+++ lf/lure.sh
@@ -20,8 +20,16 @@ version() {
 package() {
 	vers="$(curl --silent "https://api.github.com/repos/${git_repo}/releases/latest" | grep -Eo '"tag_name": "(r.*)"' | sed -E 's/.*"([^"]+)".*/\1/')"
 	echo Installing ${name} ${vers}
-	curl -L "https://github.com/${git_repo}/releases/download/${vers}/${name}-linux-${architectures}.tar.gz" -o ${name}.tar.gz
+	curl -L "https://github.com/${git_repo}/releases/download/${vers}/${name}-linux-${ARCH}.tar.gz" -o ${name}.tar.gz
+	curl -L "https://raw.githubusercontent.com/${git_repo}/master/${name}.1" -o ${name}.1
+	curl -L "https://raw.githubusercontent.com/${git_repo}/master/etc/${name}.fish" -o ${name}.fish
+	curl -L "https://raw.githubusercontent.com/${git_repo}/master/etc/${name}.zsh" -o ${name}.zsh
+	curl -L "https://raw.githubusercontent.com/${git_repo}/master/etc/${name}.bash" -o ${name}.bash
 	tar xzf "${name}.tar.gz" -C .
 	install -Dm755 "./${name}" "${pkgdir}/usr/local/bin/${name}"
+	install -Dm644 "./${name}.1" "${pkgdir}/usr/local/share/man/man1/${name}.1"
+	install -Dm644 "./${name}.fish" "${pkgdir}/usr/share/fish/completions/${name}.fish"
+	install -Dm644 "./${name}.zsh" "${pkgdir}/usr/share/zsh/site-functions/_${name}"
+	install -Dm644 "./${name}.bash" "${pkgdir}/usr/share/bash-completion/completions/${name}"
 }
  

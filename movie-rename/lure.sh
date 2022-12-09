5e9db036007e87ff90db2129aa118a11221fb350
SinTan1729 <sayantan.santra689@gmail.com>
2022-12-09 15:22:46 -0600

    Renamed movie_rename to movie-rename

--- movie-rename/lure.sh
+++ movie-rename/lure.sh
@@ -1,14 +1,14 @@
 # Maintainer: Sintan Santorum <c1scu0hh at anonaddy dot me>
-name="movie_rename"
-version=1.1.2
-release=2
+name="movie-rename"
+version=1.2.0
+release=1
 desc="A simple tool to rename movies, written in Rust."
 architectures=('amd64')
 homepage='https://github.com/SinTan1729/movie_rename'
 license=('GPL3')
-provides=('movie_rename')
+provides=('movie-rename')
 sources=("${homepage}/releases/download/${version}/${name}.tar.gz")
-checksums=('a36d52180e26514c736e89e1d267d17b672058829d09b70d6fcb2338a916da9b')
+checksums=('07f805e2c2cf06e7b2ba21b0e4bf5d6459d3e7991267f6792a4b2116481da1b8')
 package() {
 	# Binary
 	install -Dm755 ./${name} "${pkgdir}/usr/local/bin/${name}"

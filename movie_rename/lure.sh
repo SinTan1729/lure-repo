5cfd4f0ec9c95239905f78e7e2ed132f72396191
SinTan1729 <sayantan.santra689@gmail.com>
2022-12-09 00:04:05 -0600

    Fixed typo in manpage

--- movie_rename/lure.sh
+++ movie_rename/lure.sh
@@ -1,14 +1,14 @@
 # Maintainer: Sintan Santorum <c1scu0hh at anonaddy dot me>
 name="movie_rename"
 version=1.1.2
-release=1
+release=2
 desc="A simple tool to rename movies, written in Rust."
 architectures=('amd64')
 homepage='https://github.com/SinTan1729/movie_rename'
 license=('GPL3')
 provides=('movie_rename')
 sources=("${homepage}/releases/download/${version}/${name}.tar.gz")
-checksums=('2a95ce06853123b44fb0386c82d41cd8b7248ff2480021f492b6c0bae6d01a71')
+checksums=('a36d52180e26514c736e89e1d267d17b672058829d09b70d6fcb2338a916da9b')
 package() {
 	# Binary
 	install -Dm755 ./${name} "${pkgdir}/usr/local/bin/${name}"

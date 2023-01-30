acfe0fad4c3e3e137ba53cc643a39d24544d0b89
SinTan1729 <sayantan.santra689@gmail.com>
2023-01-30 15:59:25 -0600

    Bumped topgrade version to 10.3.1

--- topgrade/lure.sh
+++ topgrade/lure.sh
@@ -1,5 +1,5 @@
 name="topgrade"
-version=10.3.0
+version=10.3.1
 release=1
 desc="Invoke the upgrade procedure of multiple package managers"
 architectures=('amd64' 'aarch64' 'armv7')
@@ -11,9 +11,9 @@ conflicts=('topgrade')
 sources_amd64=("${homepage}/releases/download/v${version}/${name}-v${version}-x86_64-unknown-linux-musl.tar.gz")
 sources_aarch64=("${homepage}/releases/download/v${version}/${name}-v${version}-aarch64-unknown-linux-musl.tar.gz")
 sources_armv7=("${homepage}/releases/download/v${version}/${name}-v${version}-armv7-unknown-linux-gnueabihf.tar.gz")
-checksums_amd64=('95cabdf54f4476bde7b5153c4c77d78ddd47468c888086bcc8749245c029305f')
-checksums_aarch64=('337557844998f2ee8f054e74b10a4dc213b5442034a4c86e1bcb50c925634e0c')
-checksums_armv7=('3e74114bfdf4b8fef744cca34fcff3a4e89a4989a822936fd5ed76f7ad88dcca')
+checksums_amd64=('c561de3c965e618b9af72ffcba9cecdaaf76027b0e88954fa8328267d433b6a0')
+checksums_aarch64=('b3ef6a99fdd4777f440402b2d708276b03f7ade80b61a0d73a7039b6896d62c7')
+checksums_armv7=('9702515aa7ea31d33f2c1b2a2d1f766adab8a6915f2ac499076528305a381c5d')
 package() {
 	# binary
 	install -Dm755 ./topgrade "$pkgdir/usr/local/bin/topgrade"

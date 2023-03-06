37c3dd5e5a9abd42d4ed17e4264f0b53827700a0
SinTan1729 <sayantan.santra689@gmail.com>
2023-03-06 10:20:47 -0600

    Fix unzip location

--- vuetorrent/lure.sh
+++ vuetorrent/lure.sh
@@ -1,6 +1,6 @@
 name='vuetorrent'
 version=VERSION
-release=3
+release=4
 desc='The sleekest looking WEBUI for qBittorrent made with Vuejs!'
 homepage='https://github.com/WDaan/VueTorrent'
 architectures=('amd64')
@@ -22,8 +22,8 @@ package() {
 	echo "Pulling ${name} ${version}"
 	curl -L "https://github.com/${git_repo}/releases/latest/download/${name}.zip" -o ${name}.zip
 	# Unzip and install
-	mkdir -p "${pkgdir}/opt/${name}"
-	unzip -qq vuetorrent.zip -d "${pkgdir}/opt/${name}"
+	mkdir -p "${pkgdir}/opt"
+	unzip -qq vuetorrent.zip -d "${pkgdir}/opt"
 	# Print usage instructions
 	echo "Make sure to choose /opt/vuetorrent as the location of the custom WebUI in  qBittorrent settings."
 	echo "You might nedd to mount this directory first if you're using docker."

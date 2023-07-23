99155b1113a2e54674b40db459b51d6e671d262c
SinTan1729 <sayantan.santra689@gmail.com>
2023-07-23 18:22:10 -0500

    Bumped topgrade version to 12.0.1

--- topgrade/lure.sh
+++ topgrade/lure.sh
@@ -1,5 +1,5 @@
 name="topgrade"
-version=12.0.0
+version=12.0.1
 release=1
 desc="Invoke the upgrade procedure of multiple package managers"
 architectures=('amd64' 'aarch64' 'armv7')
@@ -11,9 +11,9 @@ conflicts=('topgrade')
 sources_amd64=("${homepage}/releases/download/v${version}/${name}-v${version}-x86_64-unknown-linux-musl.tar.gz")
 sources_aarch64=("${homepage}/releases/download/v${version}/${name}-v${version}-aarch64-unknown-linux-musl.tar.gz")
 sources_armv7=("${homepage}/releases/download/v${version}/${name}-v${version}-armv7-unknown-linux-gnueabihf.tar.gz")
-checksums_amd64=('54bd17b1b72d0e4f39159ea8f4db9d42f081c1c97dcfc91f679d2e2bd13963bd')
-checksums_aarch64=('639897e504b7b114099c13538bfa1aff43215ac1fb205a5bc322dfeb02ee2ecf')
-checksums_armv7=('9ee834778fa2727c533cd1731f0edff1a3c2c53de869544d8e6549daadc2bb44')
+checksums_amd64=('40b1e1da5f341f9bf7eb996ac20f9581746f82222adb86438100605bec8b770c')
+checksums_aarch64=('2f36d8e2da7dfa46fe39b006e3d740268d2e55fc54647af7baf12647cd4d5e84')
+checksums_armv7=('a15929b2de0a6b1ce4d6e8ee4d7f18bf9aa11e1619b7f469f97d1ace26f9283c')
 package() {
 	# install binary
 	install -Dm755 ./topgrade "$pkgdir/usr/local/bin/topgrade"

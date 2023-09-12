05600eba1a4ec7a37f222e1306155449b6dee19a
SinTan1729 <sayantan.santra689@gmail.com>
2023-09-12 16:41:55 -0500

    new: Add fastfetch

---
+++ fastfetch/lure.sh
@@ -0,0 +1,28 @@
+name='fastfetch'
+version=VERSION
+release=1
+desc='Like neofetch, but much faster because written in C.'
+homepage='https://github.com/fastfetch-cli/fastfetch'
+architectures=('amd64')
+maintainer='SinTan1729'
+license=('APACHE-2.0' 'MIT')
+provides=('fastfetch', 'flashfetch')
+conflicts=('fastfetch', 'flashfetch')
+git_repo='fastfetch-cli/fastfetch'
+
+sources=()
+checksums=()
+
+version() {
+	printf "$(curl -sL "https://api.github.com/repos/${git_repo}/releases/latest" | jq -r '.tag_name')"
+}
+
+package() {
+	# Pull sources
+	echo Pulling ${name} ${version}
+	curl -L "https://github.com/${git_repo}/releases/latest/download/${name}-${version}-Linux.rpm" -o ${name}.rpm
+	# Build package
+	echo Creating the package
+	cd ${pkgdir}
+	rpm2cpio ../${name}.rpm | zstd -d | cpio -idmv
+}

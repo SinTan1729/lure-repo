6018be51d15efad072b8cf45726a1701d84dccd3
SinTan1729 <sayantan.santra689@gmail.com>
2023-09-13 18:31:31 -0500

    Bumped movie-rename version to 2.1.5

--- movie-rename/lure.sh
+++ movie-rename/lure.sh
@@ -8,7 +8,7 @@ homepage='https://github.com/SinTan1729/movie_rename'
 license=('GPL3')
 provides=('movie-rename')
 sources_amd64=("${homepage}/releases/download/${version}/${name}.tar.gz")
-checksums_amd64=('13daa515e133fecdcf94363631777f70363ba49474adddd9f9c536fbbef83c44')
+checksums_amd64=('4848301127012846f880327e5ad4d049d0dfd5bd120c9e1cc64ba42ede779bfb')
 package() {
 	# Binary
 	install-binary "./${name}"

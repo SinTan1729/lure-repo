0fd3ec64e7f45595f2f0eaad2608df718acd77ad
SinTan1729 <sayantan.santra689@gmail.com>
2024-10-10 22:15:23 -0500

    Bumped topgrade version to 16.0.1

--- topgrade/lure.sh
+++ topgrade/lure.sh
@@ -1,5 +1,5 @@
 name="topgrade"
-version=16.0.0
+version=16.0.1
 release=1
 desc="Invoke the upgrade procedure of multiple package managers"
 architectures=('amd64' 'aarch64' 'armv7')
@@ -11,9 +11,9 @@ conflicts=('topgrade')
 sources_amd64=("${homepage}/releases/download/v${version}/${name}-v${version}-x86_64-unknown-linux-musl.tar.gz")
 sources_aarch64=("${homepage}/releases/download/v${version}/${name}-v${version}-aarch64-unknown-linux-musl.tar.gz")
 sources_armv7=("${homepage}/releases/download/v${version}/${name}-v${version}-armv7-unknown-linux-gnueabihf.tar.gz")
-checksums_amd64=('8c10135cd6c569d8adc27ec1b46b359e70954de5763bad1ba42483e0ff0e8a88')
-checksums_aarch64=('46fbe915d024e1286055989857ffc213fdb234b5e15d20a4e71548f06867f8d1')
-checksums_armv7=('0494888725d45e20c7e96338032ed7b33fd18728d69e3eb58e10db040d1fbf95')
+checksums_amd64=('61c9f61c4078f9b925e4f5eacbdf056b934c961d7279def368436b7a52f50b83')
+checksums_aarch64=('376313d6f15acceb2d459c663daa8c77da0b28a92aab0a998ffa6beba2562254')
+checksums_armv7=('b963ea528d8ec9efcf88c9df5c0056241230b2944e066e141146b67da32f655b')
 package() {
 	# install binary
 	install-binary "./${name}"

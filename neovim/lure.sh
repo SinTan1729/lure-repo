44c3b6c5acdd4ed169e43a7305ee9eb59c2e6873
SinTan1729 <sayantan.santra689@gmail.com>
2025-01-30 19:26:58 -0600

    fix: neovim tar name

--- neovim/lure.sh
+++ neovim/lure.sh
@@ -20,7 +20,7 @@ version() {
 package() {
 	# Pull sources
 	echo "Pulling ${name} ${version}"
-	curl -L "https://github.com/${git_repo}/releases/latest/download/nvim-linux64.tar.gz" -o ${name}.tar.gz
+	curl -L "https://github.com/${git_repo}/releases/latest/download/nvim-linux-x86_64.tar.gz" -o ${name}.tar.gz
 	# Build package
 	echo "Creating the package"
 	tar -xzf "${name}.tar.gz" -C .

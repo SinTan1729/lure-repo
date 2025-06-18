ebbeb72cc3759b6853caee5a39a3723cfa231007
SinTan1729 <sayantan.santra689@gmail.com>
2025-06-18 17:29:00 -0500

    fix: Filename

--- chhoto-url-cli/lure.sh
+++ chhoto-url-cli/lure.sh
@@ -6,7 +6,7 @@ architectures=('amd64')
 homepage='https://github.com/SinTan1729/chhoto-url-cli'
 license=('MIT')
 provides=('chhoto')
-sources_amd64=("${homepage}/releases/download/${version}/${name}.tar.gz")
+sources_amd64=("${homepage}/releases/download/${version}/chhoto.tar.gz")
 checksums_amd64=('6d1b862cba47abfb52b42af1630f85c783935451d4d78c2df0f1dc43fd257f66')
 package() {
 	# Binary

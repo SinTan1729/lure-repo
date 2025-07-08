af063ad4fc46ba3fb6ed4a91aca0d8b776c98ad2
SinTan1729 <sayantan.santra689@gmail.com>
2025-07-08 02:24:22 -0500

    Bumped chhoto-url-cli version to 0.4.1

--- chhoto-url-cli/lure.sh
+++ chhoto-url-cli/lure.sh
@@ -1,5 +1,5 @@
 name="chhoto-url-cli"
-version=0.4.0
+version=0.4.1
 release=1
 desc="A CLI tool to be used to interact with Chhoto URL."
 architectures=('amd64')
@@ -7,7 +7,7 @@ homepage='https://github.com/SinTan1729/chhoto-url-cli'
 license=('MIT')
 provides=('chhoto')
 sources_amd64=("${homepage}/releases/download/${version}/chhoto.tar.gz")
-checksums_amd64=('f36b0d80956ccb7c3bfaff996c629d29e9cc91cd787b11628a3e08efe9b68eab')
+checksums_amd64=('c425f91fdb148e2c5fd58fa56c18ac0bfdf4bf462c8b03a9db0e26ad1101a172')
 package() {
 	# Binary
 	install-binary "./chhoto"

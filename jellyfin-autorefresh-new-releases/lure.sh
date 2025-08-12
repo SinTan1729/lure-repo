7b1bda9c97c74dcb927406fcb60cac042bd7b145
SinTan1729 <sayantan.santra689@gmail.com>
2025-08-11 21:50:30 -0500

    new: Added jellyfin-autorefresh-new-releases

---
+++ jellyfin-autorefresh-new-releases/lure.sh
@@ -0,0 +1,16 @@
+name="jellyfin-autorefresh-new-releases"
+version=0.1.0
+release=1
+desc="Get missing metadata for new releases in Jellyfin"
+architectures=('amd64')
+homepage="https://github.com/SinTan1729/jellyfin-autorefresh-new-releases"
+license=('GPL3')
+provides=('jellyfin-autorefresh')
+sources_amd64=("${homepage}/releases/download/${version}/jellyfin-autorefresh-new-releases.tar.gz")
+checksums_amd64=("3ee139b801210f935c49be8d0a73b7ef3301fd2a36201bccfeed3785b7f97744")
+package() {
+	# Binary
+	install-binary "./jellyfin-autorefresh"
+	# Manpage
+	install-manual "./jellyfin-autorefresh.1"
+}

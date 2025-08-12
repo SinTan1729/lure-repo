9ff2b293158ca78b021815208665e96862658429
SinTan1729 <sayantan.santra689@gmail.com>
2025-08-12 01:26:45 -0500

    Bumped jellyfin-autorefresh-new-releases version to 0.1.2

--- jellyfin-autorefresh-new-releases/lure.sh
+++ jellyfin-autorefresh-new-releases/lure.sh
@@ -1,5 +1,5 @@
 name="jellyfin-autorefresh-new-releases"
-version=0.1.1
+version=0.1.2
 release=1
 desc="Get missing metadata for new releases in Jellyfin"
 architectures=('amd64')
@@ -7,7 +7,7 @@ homepage="https://github.com/SinTan1729/jellyfin-autorefresh-new-releases"
 license=('GPL3')
 provides=('jellyfin-autorefresh')
 sources_amd64=("${homepage}/releases/download/${version}/jellyfin-autorefresh.tar.gz")
-checksums_amd64=('88e1956e7fa3c06b33528f909e90d4b39f7e86d37ce213664f0724226c059532')
+checksums_amd64=('e2ec68a972a2337d49fcefbae626ca490cab3d6702983c9ddc25babc36de8678')
 package() {
 	# Binary
 	install-binary "./jellyfin-autorefresh"

63b0c4af9ad82be2b8ce000554de68aa14fd6df1
SinTan1729 <sayantan.santra689@gmail.com>
2026-03-12 11:52:12 -0500

    Bumped jellyfin-autorefresh-new-releases version to 0.2.7

--- jellyfin-autorefresh-new-releases/lure.sh
+++ jellyfin-autorefresh-new-releases/lure.sh
@@ -7,7 +7,7 @@ homepage="https://github.com/SinTan1729/jellyfin-autorefresh-new-releases"
 license=('GPL3')
 provides=('jellyfin-autorefresh')
 sources_amd64=("${homepage}/releases/download/${version}/jellyfin-autorefresh.tar.gz")
-checksums_amd64=('6af1ab8dba18b5e94775bbd5eba0b86564c6b5cd419af9305633c50bf5c7113b')
+checksums_amd64=('df296f68fce1a4ecf535c454f780ee50f6877b05bcd237d6de88b34245c114ba')
 package() {
 	# Binary
 	install-binary "./jellyfin-autorefresh"

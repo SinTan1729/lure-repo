93e6cfb5700974a6f4b4036b5e2e349380bc2ca3
SinTan1729 <sayantan.santra689@gmail.com>
2026-03-28 15:19:20 -0500

    new: Added tree-sitter

---
+++ tree-sitter/lure.sh
@@ -0,0 +1,30 @@
+name='tree-sitter-cli'
+_name='tree-sitter'
+version=VERSION
+release=1
+desc='An incremental parsing system for programming tools'
+homepage='https://github.com/tree-sitter/tree-sitter'
+architectures=('amd64' 'arm64')
+maintainer='SinTan1729'
+license=('MIT')
+provides=('tree-sitter')
+conflicts=('tree-sitter')
+git_repo='tree-sitter/tree-sitter'
+
+sources=()
+checksums=()
+
+version() {
+	printf "$(curl -sL "https://api.github.com/repos/${git_repo}/releases/latest" | jq -r '.tag_name')"
+}
+
+package() {
+	# Pull sources
+	echo Pulling ${name} ${version}
+	[ ${ARCH} == "arm64" ] && ARCH="aarch64"
+	curl -L "https://github.com/${git_repo}/releases/latest/download/${name}-linux-${ARCH}.zip" -o ${name}.zip
+	# Build package
+	echo Creating the package
+	unzip "${name}.zip"
+	install-binary "./${_name}"
+}

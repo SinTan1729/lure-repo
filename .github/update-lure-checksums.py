a80c9dd4e88497efb123b7e45106e7f51ea24f25
SinTan1729 <sayantan.santra689@gmail.com>
2026-06-19 12:09:03 -0500

    chg: Rewrote the lure update script

--- .github/update-lure-checksums.py
+++ .github/update-lure-checksums.py
@@ -1,153 +1,207 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 # This is a script to update checksums for sources in lure build scripts
 # This is a crude analog for updpkgsums for PKGBUILD files
 # The script assumed that matching sources* and checksums* lines are already there
+# --force will always update the checksums
 
 import os
+import tempfile
 import sys
-import re
-import time
+import platform
 import requests
 import hashlib
-import platform
 
-session = requests.Session()
+# Global variables
+force: bool = False
+name: str | None = None
+old_version: str | None = None
+git_repo: str | None = None
+sources: dict[str, list[str]] = {}
+checksums: dict[str, list[str]] = {}
+sys_arch: str
 
 
-def get_file_checksum(url: str) -> str:
+def unquote(s: str) -> str:
+    if s.startswith("'") and s.endswith("'"):
+        s = s[1:-1]
+    if s.startswith('"') and s.endswith('"'):
+        s = s[1:-1]
+    return s
+
+
+def expand_vars(s: str, source: str, version: str) -> str:
+    if name is not None:
+        s = s.replace("${name}", name)
+        s = s.replace("$name", name)
+    if version is not None:
+        s = s.replace("${version}", version)
+        s = s.replace("$version", version)
+    if git_repo is not None:
+        s = s.replace("${git_repo}", git_repo)
+        s = s.replace("$git_repo", git_repo)
+    if source == "any":
+        source = sys_arch
+    s = s.replace("${ARCH}", source)
+    s = s.replace("$ARCH", source)
+
+    if "$" in s:
+        raise IndexError(f"Unable to expand all vars in: {s}")
+    return s
+
+
+def read_args(args):
+    global force, sys_arch
+    if "--force" in args:
+        force = True
+
+    sys_arch = platform.machine().lower()
+    if sys_arch == "x86_64":
+        sys_arch = "amd64"
+
+
+def read_vars():
+    global name, old_version, git_repo, sources, checksums
+    with open("lure.sh") as f:
+        while line := f.readline():
+            line = line.rstrip("\n").strip()
+            if "=" not in line:
+                continue
+
+            var, val = line.split("=", 1)
+            match var:
+                case "name":
+                    name = unquote(val)
+                case "version":
+                    old_version = unquote(val)
+                case "git_repo":
+                    git_repo = unquote(val)
+                    continue
+            if "_" not in var and var not in ["sources", "checksums"]:
+                continue
+
+            try:
+                entry, arch = var.split("_", 1)
+            except ValueError:
+                entry, arch = var, "any"
+            values: list[str] = []
+            if ")" in line:
+                values.append(unquote(line.split("(", 1)[1].rstrip(")")))
+            else:
+                while True:
+                    line = f.readline()
+                    if line.startswith(")"):
+                        break
+                    values.append(unquote(line.rstrip("\n").strip()))
+
+            if entry == "sources":
+                sources[arch] = values
+            elif entry == "checksums":
+                checksums[arch] = values
+
+
+def safe_get(ses: requests.Session, url: str) -> dict:
+    req = ses.get(url)
+    req.raise_for_status()
+    return req.json()
+
+
+def get_latest_version(ses: requests.Session) -> str:
+    if git_repo is None:
+        sys.exit("Unable to get latest version. No github repo provided!")
+    vers: str | None = safe_get(
+        ses, f"https://api.github.com/repos/{git_repo}/releases/latest"
+    ).get("tag_name")
+    if vers is not None and vers.startswith("v"):
+        vers = vers[1:]
+    if vers is None:
+        sys.exit("Unable to get latest version. No github repo provided!")
+    return vers
+
+
+def get_source_checksum(ses: requests.Session, url: str, source: str, version: str) -> str:
+    url = expand_vars(url, source, version)
     if "github.com" in url and "/releases/download" in url:
         _, _, _, owner, repo, _, _, tag, filename = url.split("/")
         return next(
-            a["digest"].split("sha256:", 2)[1]
-            for a in requests.get(
+            a["digest"].split("sha256:", 1)[1]
+            for a in ses.get(
                 f"https://api.github.com/repos/{owner}/{repo}/releases/tags/{tag}"
             ).json()["assets"]
             if a["name"] == filename
         )
 
-    r = session.get(url, stream=True, timeout=30)
-    r.raise_for_status()
-
+    r = ses.get(url, stream=True, timeout=30)
     sha256 = hashlib.sha256()
-
     for chunk in r.iter_content(chunk_size=8192):
         if chunk:
             sha256.update(chunk)
-
     return sha256.hexdigest()
 
 
-# Check if the build script exists
-if not os.path.isfile("lure.sh"):
-    print("Couldn't find a lure.sh file in the current directory.")
-    exit(1)
+def write_file(target_version):
+    with tempfile.NamedTemporaryFile("w", delete=False, dir=".") as out:
+        with open("lure.sh") as f:
+            while line := f.readline():
+                if line.startswith("version="):
+                    out.write(f"version={target_version}\n")
+                elif line.startswith("checksums"):
+                    prefix = line.split("=", 1)[0]
+                    try:
+                        arch = prefix.split("_", 1)[1]
+                        suffix = "_" + arch
+                    except IndexError:
+                        arch = "any"
+                        suffix = ""
+                    if len(checksums[arch]) == 1:
+                        out.write(f"checksums{suffix}=('{checksums[arch][0]}')\n")
+                    else:
+                        out.write(f"checksums{suffix}=(\n")
+                        for checksum in checksums[arch]:
+                            out.write(f"    '{checksum}'\n")
+                        out.write(")\n")
+                        while not line.startswith(")"):
+                            line = f.readline()
+                else:
+                    out.write(line)
+    os.replace(out.name, "lure.sh")
 
-with open("lure.sh", "r") as script_file:
-    script = script_file.readlines()
 
-vars = {}
-multiline_var = None
-for line in script:
-    if multiline_var is None and not line.startswith(
-        ("name=", "version=", "git_repo=", "sources", "checksums")
-    ):
-        continue
-    if line.startswith(")"):
-        multiline_var = None
-        continue
-    if multiline_var is not None:
-        vars[multiline_var] = f"{vars[multiline_var]} {line.strip()}"
-        continue
-    if line.startswith(("sources_", "checksums_")) and line.strip("\n").endswith("("):
-        multiline_var = line.split("=", 2)[0]
-        vars[multiline_var] = ""
-    else:
-        multiline_var = None
-        var, val = line.strip("\n").split("=", 2)
-        vars[var] = val.strip('""').strip("''")
+def main(args: list[str]):
+    read_args(args)
+    read_vars()
 
-arch = platform.machine().lower()
-if arch == "x86_64":
-    arch = "amd64"
-vars["ARCH"] = arch
+    if name is None:
+        raise ValueError("No name found!")
+    print(f"Procesing {name}...")
+    if old_version is None:
+        raise ValueError("No version found!")
+    print(f"Detected version: {old_version}")
+    if len(sources) != len(checksums):
+        raise IndexError("Sources and checksums are not one-to-one!")
+    if not sources:
+        print("No sources to update.")
+        sys.exit()
 
-print(f"Detected version: {vars['version']}")
-new_version = None
-if len(sys.argv) > 1 and sys.argv[1] == "--upgrade":
-    if vars.get("git_repo") is None:
-        print("No github repo found for processing upgrade.")
-    new_version = session.get(
-        f"https://api.github.com/repos/{vars['git_repo']}/releases/latest"
-    ).json()["tag_name"]
-    if new_version[0] == "v":
-        new_version = new_version[1:]
-    if new_version != vars["version"]:
-        vars["version"] = new_version
-        print(f"Found new version {new_version}")
-    else:
-        new_version = None
+    session = requests.session()
+    target_version = get_latest_version(session)
+    if old_version == target_version and not force:
+        print("Package is up-to-date.")
+        sys.exit()
+    elif not force:
+        print("Update available.")
+        print(f"{name}: {old_version} -> {target_version}")
 
-if new_version is None and not (len(sys.argv) > 2 and sys.argv[2] == "--force"):
-    print("The package is up-to-date.")
-    exit()
+    for source in sources.keys():
+        for i, item in enumerate(sources[source]):
+            if checksums[source][i] == "SKIP":
+                continue
+            checksum = get_source_checksum(session, item, source, target_version)
+            checksums[source][i] = checksum
 
-for src_type in [k for k in vars.keys() if "sources" in k]:
-    try:
-        suffix = src_type.split("_")[1]
-    except IndexError:
-        suffix = ""
-    if suffix != "":
-        vars["ARCH"] = suffix
-        suffix = "_" + suffix
+    write_file(target_version)
+    print("Updated package.")
 
-    # Read all the links
-    links = [link.strip('""').strip("''") for link in vars[src_type].strip("()").split()]
 
-    # Get the old sums
-    old_sums = vars["checksums" + suffix].strip("()").split()
-    old_sums = [sum.strip('""').strip("''") for sum in old_sums]
-
-    checksums = []
-    for i, link in enumerate(links):
-        if old_sums[i] == "SKIP":
-            checksums.append("SKIP")
-            continue
-        # Try to do variable expansions (works up to one level, should be enough)
-        to_replace = list(set(re.findall(r"\$\{\w+\}", link)))
-        for str in to_replace:
-            str_clean = str.strip("${}")
-            link = link.replace(str, vars[str_clean])
-
-        checksum = get_file_checksum(link)
-        checksums.append(checksum)
-
-    # Build the output line
-    if len(checksums) > 1:
-        sum_out = f"checksums{suffix}=(\n    '{"'\n    '".join(checksums)}'\n)\n"
-    else:
-        sum_out = f"checksums{suffix}=('{checksums[0]}')\n"
-    skip_next_line = False
-    for i, line in enumerate(script):
-        if skip_next_line:
-            if line.startswith(")"):
-                skip_next_line = False
-            script[i] = ""
-            continue
-        if new_version is not None and line.startswith("version="):
-            script[i] = f"version={new_version}\n"
-            print("Updated version inside script.")
-        if line.startswith(f"checksums{suffix}="):
-            if len(checksums) > 1:
-                skip_next_line = True
-            script[i] = sum_out
-            print("Updated checksums for " + suffix[1:] + " sources")
-
-timestamp = "{}".format(int(time.time()))
-
-with open(timestamp, "w") as tempfile:
-    tempfile.writelines(script)
-
-os.rename(timestamp, "lure.sh")
-
-print("Done!")
+if __name__ == "__main__":
+    main(sys.argv)

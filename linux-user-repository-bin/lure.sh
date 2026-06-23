c2f75def7427c0c30a50642b95c256c20cbb7a7e
SinTan1729 <sayantan.santra689@gmail.com>
2026-06-22 22:42:00 -0500

    fix: Package structure

--- linux-user-repository-bin/lure.sh
+++ linux-user-repository-bin/lure.sh
@@ -1,5 +1,5 @@
 name='linux-user-repository-bin'
-version=0.1.8
+version=0.1.9
 release=1
 desc='Linux User REpository'
 homepage='https://lure.sh'
@@ -10,40 +10,26 @@ provides=('linux-user-repository')
 conflicts=('linux-user-repository' 'linux-user-repository-bin' 'linux-user-repository-git')
 git_repo='SinTan1729/lure'
 
-sources_arm64=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-aarch64.tar.gz")
-checksums_arm64=('8bc0c538b8123bfafc6e33d675a11089f135e3ee8a259e8ef55c2d704dd4ff24')
+sources_arm64=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-${ARCH}.tar.gz")
+checksums_arm64=('66f1b64da74f5ec4683106a2af02ffe063cf6ba177f8e2a8e976c2282a4fe98a')
 
-sources_arm=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-arm.tar.gz")
-checksums_arm=('300e509e1ac1c24112365bd450e54a3d4287fc01b597cde44bcf61517287c414')
+sources_arm=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-${ARCH}.tar.gz")
+checksums_arm=('35824602866882c34fc439e1efbe1f0c8dc20282d95be80b8d1df2c38cf49a52')
 
-sources_amd64=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-x86_64.tar.gz")
-checksums_amd64=('215ef6842a9098059dde49ace961c191d86f22e4d24b09a8aeb91708b3ac2559')
+sources_amd64=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-${ARCH}.tar.gz")
+checksums_amd64=('67d442a322552019d28959804e1b3f31956e0cc2e77610bee5dad04ba76522d6')
 
-sources_386=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-i386.tar.gz")
-checksums_386=('8e5d72366071be3d8bd6370c4ea32a55e8c73d9629c6da687c0ed54ddd773462')
+sources_386=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-${ARCH}.tar.gz")
+checksums_386=('ea6fb378e49c77f2fec704428bff28ee604b34b837374e8e5a12101fb20ecb3d')
 
-sources_riscv64=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-riscv64.tar.gz")
-checksums_riscv64=('2e790b42254d9f4fcc6a48f71950eade4003254b8c2240b5c64317f2d4596cdd')
+sources_riscv64=("https://github.com/SinTan1729/lure/releases/download/${version}/lure-v${version}-linux-${ARCH}.tar.gz")
+checksums_riscv64=('e83c02de1924fe89a30824ea3471962341f284b049ec38fc7325800053b47446')
 
 package() {
-    case $ARCH in
-    arm64)
-        tmp_arch=aarch64
-        ;;
-    amd64)
-        tmp_arch=x86_64
-        ;;
-    386)
-        tmp_arch=i386
-        ;;
-    *)
-        tmp_arch=$ARCH
-        ;;
-    esac
-    cp -l "${srcdir}/lure-v${version}-linux-${tmp_arch}" "${srcdir}/lure"
-    install-binary ${srcdir}/lure
-
-    install-manual "${srcdir}/lure.1"
-    install-completion bash lure <"${srcdir}/completion/bash"
-    install-completion zsh lure <"${srcdir}/completion/zsh"
+    workdir="${srcdir}/lure-v${version}-linux-${ARCH}"
+    install-binary ${workdir}/lure
+    install-manual "${workdir}/misc/man/lure.1"
+    install-completion bash lure <"${workdir}/misc/completion/lure.bash"
+    install-completion fish lure <"${workdir}/misc/completion/lure.fish"
+    install-completion zsh lure <"${workdir}/misc/completion/_lure"
 }
